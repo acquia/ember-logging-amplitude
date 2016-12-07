cc0b6786536d65fb910f3dd2983022e067fffa7d
Lisa Backer <lisa.backer@acquia.com>
2016-12-07 08:04:51 -0500

    Fixing naming and timing issues

--- app/instance-initializers/ember-logging-amplititude.js
+++ app/instance-initializers/ember-logging-amplititude.js
@@ -1,11 +1,12 @@
 import ENV from '../config/environment';
-import registerLoggingConsumer from 'ember-logging-amplititude/instance-initializers/register-logging-consumer';
+import registerLoggingConsumer from 'ember-logging-amplitude/instance-initializers/register-logging-consumer';
 
 export function initialize(instance) {
   registerLoggingConsumer(instance, ENV);
 }
 
 export default {
-  name: 'ember-logging-amplititude',
+  name: 'ember-logging-amplitude',
+  after: 'ember-logging-service',
   initialize
 };
